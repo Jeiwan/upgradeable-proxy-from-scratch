aecd42585a644df5ec837d2c1cac776bcbc460fe
Ivan Kuznetsov <me@jeiwan.ru>
2021-08-12 14:21:16 +0300

    Finish Logic and Proxy

--- contracts/Logic.sol
+++ contracts/Logic.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.8.4;
 
 contract Logic {
+    bool initialized;
     uint256 magicNumber;
 
-    constructor() {
+    function initialize() public {
+        require(!initialized, "already initialized");
+
         magicNumber = 0x42;
+        initialized = true;
     }
 
     function setMagicNumber(uint256 newMagicNumber) public {
---
+++ test/Proxy.test.js
@@ -0,0 +1,50 @@
+require("@nomiclabs/hardhat-waffle");
+const { expect } = require("chai");
+
+describe("Proxy", async () => {
+  let owner;
+  let proxy, logic;
+
+  beforeEach(async () => {
+    [owner] = await ethers.getSigners();
+
+    const Logic = await ethers.getContractFactory("Logic");
+    logic = await Logic.deploy();
+    await logic.deployed();
+
+    const Proxy = await ethers.getContractFactory("Proxy");
+    proxy = await Proxy.deploy();
+    await proxy.deployed();
+
+    await proxy.setImplementation(logic.address);
+
+    const abi = ["function initialize() public"];
+    const proxied = new ethers.Contract(proxy.address, abi, owner);
+
+    await proxied.initialize();
+  });
+
+  it("points to an implementation contract", async () => {
+    expect(await proxy.callStatic.getImplementation()).to.eq(logic.address);
+  });
+
+  it("proxies calls to implementation contract", async () => {
+    abi = [
+      "function setMagicNumber(uint256 newMagicNumber) public",
+      "function getMagicNumber() public view returns (uint256)",
+    ];
+
+    const proxied = new ethers.Contract(proxy.address, abi, owner);
+
+    expect(await proxied.getMagicNumber()).to.eq("0x42");
+  });
+
+  it("cannot be initialized twice", async () => {
+    abi = ["function initialize() public"];
+    const proxied = new ethers.Contract(proxy.address, abi, owner);
+
+    await expect(proxied.initialize()).to.be.revertedWith(
+      "already initialized"
+    );
+  });
+});
